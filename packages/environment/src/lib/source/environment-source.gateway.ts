273f26a8de3504127ec82467ad220eeca07b4e22
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-01-16 16:31:14 +0000

    feat(enfironment): add source submodule

---
+++ packages/environment/src/lib/source/environment-source.gateway.ts
@@ -0,0 +1,46 @@
+import { ObservableInput } from 'rxjs';
+
+import { Path } from '../path';
+import { EnvironmentState } from '../store';
+
+/**
+ * The source from which to get environment properties asynchronously.
+ */
+export abstract class EnvironmentSource {
+  /**
+   * The source name.
+   */
+  name?: string;
+
+  /**
+   * Loads the required to load sources properties before the environment load.
+   */
+  requiredToLoad?: boolean;
+
+  /**
+   * Loads the source in the declaration order.
+   * The ordered sources will wait until the previous ordered source completes to start.
+   */
+  loadInOrder?: boolean;
+
+  /**
+   * Adds properties to the environment using the deep merge strategy.
+   */
+  mergeProperties?: boolean;
+
+  /**
+   * Ignores the errors thrown by the source.
+   */
+  ignoreError?: boolean;
+
+  /**
+   * The path to set the properties in the environment.
+   * @see Path
+   */
+  path?: Path;
+
+  /**
+   * Asynchronously loads the environment properties from the source.
+   */
+  abstract load(): ObservableInput<EnvironmentState>;
+}
---
+++ packages/environment/src/lib/source/index.ts
@@ -0,0 +1,5 @@
+/**
+ * > The source from which to get environment properties asynchronously.
+ * @module EnvironmentSource
+ */
+export * from './environment-source.gateway';
