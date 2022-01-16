5144b4fbdadba90bb76eb3c8377556c7df02682e
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-01-16 13:01:55 +0000

    chore(environment): update settings

--- packages/environment/src/index.ts
+++ packages/environment/src/index.ts
@@ -1,7 +1,8 @@
 /**
  * @module Environment
  */
-export * from './lib/mutation/index';
+export * from './lib/helpers/index';
 export * from './lib/path/index';
-export * from './lib/service';
+export * from './lib/query/index';
+export * from './lib/service/index';
 export * from './lib/store/index';
