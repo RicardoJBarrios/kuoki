28456b493a83b747a5ef85ade96d14d8ba2e7731
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-01-19 19:13:23 +0000

    feat(environment): add create loader function

---
+++ packages/environment/src/lib/loader/create-environment-loader.function.ts
@@ -0,0 +1,19 @@
+import { ArrayOrSingle } from 'ts-essentials';
+
+import { EnvironmentService } from '../service';
+import { EnvironmentSource } from '../source';
+import { EnvironmentLoader } from './environment-loader.application';
+
+/**
+ * Creates an EnvironmentLoader.
+ * @param service The environment service.
+ * @param sources The sources to get environment properties.
+ * @returns An EnvironmentLoader instance.
+ * @see {@link EnvironmentLoader}
+ */
+export function createEnvironmentLoader(
+  service: EnvironmentService,
+  sources?: ArrayOrSingle<EnvironmentSource>,
+): EnvironmentLoader {
+  return new EnvironmentLoader(service, sources);
+}
