3f6e1695c66e2b13e0a1336b979161cd30542968
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-02-08 22:52:36 +0000

    feat(environment): export loader and source

--- packages/environment/src/index.ts
+++ packages/environment/src/index.ts
@@ -2,7 +2,9 @@
  * @module Environment
  */
 export * from './lib/helpers/index';
+export * from './lib/loader/index';
 export * from './lib/path/index';
 export * from './lib/query/index';
 export * from './lib/service/index';
+export * from './lib/source/index';
 export * from './lib/store/index';
