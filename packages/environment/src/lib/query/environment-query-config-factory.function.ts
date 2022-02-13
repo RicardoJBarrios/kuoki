58ad310f2c866b20824179c77ee6e31c544958a4
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-02-13 22:50:06 +0000

    docs(environment): update docs

--- packages/environment/src/lib/query/environment-query-config-factory.function.ts
+++ packages/environment/src/lib/query/environment-query-config-factory.function.ts
@@ -6,15 +6,6 @@ import { EnvironmentQueryConfig } from './environment-query-config.interface';
  * The default {@link EnvironmentQueryConfig} value is `{ interpolation: ['{{', '}}'], transpileEnvironment: false }`.
  * @param config The partial environment query config.
  * @returns The environment query configuration with all default values for empty parameters.
- * @example
- * ```js
- * environmentQueryConfigFactory();
- * // {interpolation:['{{','}}'],transpileEnvironment:false}
- * environmentQueryConfigFactory({ interpolation: ['(', ')'] });
- * // {interpolation:['(',')'],transpileEnvironment:false}
- * environmentQueryConfigFactory({ transpileEnvironment: true });
- * // {interpolation:['{{','}}'],transpileEnvironment:true}
- * ```
  */
 export function environmentQueryConfigFactory(config?: EnvironmentQueryConfig): Required<EnvironmentQueryConfig> {
   return {
