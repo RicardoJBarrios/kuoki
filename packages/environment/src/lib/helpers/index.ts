0191cc8aefaf14895fa53894dda9c12180434ef0
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-02-13 13:40:17 +0000

    feat(environment): export new helpers

--- packages/environment/src/lib/helpers/index.ts
+++ packages/environment/src/lib/helpers/index.ts
@@ -2,6 +2,8 @@
  * [[include:helpers/README.md]]
  * @module Helpers
  */
+export * from './as-error.function';
+export * from './as-string.function';
 export * from './async-not-nil.function';
 export * from './filter-nil.operator';
 export * from './at-least-one.type';
