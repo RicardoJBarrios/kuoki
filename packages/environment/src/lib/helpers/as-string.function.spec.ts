784a376fccb4ee1b52691ea2f5a9e626c8bd3118
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-02-13 13:39:32 +0000

    feat(environment): add asString helper

---
+++ packages/environment/src/lib/helpers/as-string.function.spec.ts
@@ -0,0 +1,41 @@
+import { asString } from './as-string.function';
+
+describe('asString(value)', () => {
+  it(`returns string from string`, () => {
+    expect(asString('a')).toEqual('a');
+  });
+
+  it(`returns string from number`, () => {
+    expect(asString(0)).toEqual('0');
+  });
+
+  it(`returns string from boolean`, () => {
+    expect(asString(true)).toEqual('true');
+  });
+
+  it(`returns string from null`, () => {
+    expect(asString(null)).toEqual('null');
+  });
+
+  it(`returns string from Array`, () => {
+    expect(asString([0, 'a', null, true])).toEqual('[0,"a",null,true]');
+  });
+
+  it(`returns string from Object`, () => {
+    expect(asString({ a: 0 })).toEqual('{"a":0}');
+  });
+
+  it(`returns string from Array of Objects`, () => {
+    expect(asString([{ a: 0 }])).toEqual('[{"a":0}]');
+  });
+
+  it(`returns string from Object with Arrays`, () => {
+    expect(asString({ a: [0, 'a', null, true] })).toEqual('{"a":[0,"a",null,true]}');
+  });
+
+  it(`returns string if TypeError converting an Object`, () => {
+    const a: any = { a: '0' };
+    a.myself = a;
+    expect(asString(a)).toEqual('[object Object]');
+  });
+});
---
+++ packages/environment/src/lib/helpers/as-string.function.ts
@@ -0,0 +1,13 @@
+/**
+ * Converts any type value to String, stringify objects.
+ * @typeParam The type of the value.
+ * @param value The value to convert.
+ * @returns The value as string.
+ */
+export function asString<T>(value: T): string {
+  try {
+    return typeof value === 'object' ? JSON.stringify(value) : String(value);
+  } catch {
+    return String(value);
+  }
+}
