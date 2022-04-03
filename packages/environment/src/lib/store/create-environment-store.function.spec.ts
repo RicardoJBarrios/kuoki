49fe90971dfec34deba18c3d6df6a364c6af092f
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-04-03 19:48:40 +0100

    test(environment): update test description

--- packages/environment/src/lib/store/create-environment-store.function.spec.ts
+++ packages/environment/src/lib/store/create-environment-store.function.spec.ts
@@ -1,13 +1,13 @@
 import { createEnvironmentStore, DefaultEnvironmentStore } from '../store';
 
 describe('createEnvironmentStore(initialState?)', () => {
-  it(`returns a DefaultEnvironmentStore with the default initial state '{}' `, () => {
+  it(`() returns a DefaultEnvironmentStore with the default initial state '{}' `, () => {
     const store: any = createEnvironmentStore();
     expect(store).toBeInstanceOf(DefaultEnvironmentStore);
     expect(store.initialState).toEqual({});
   });
 
-  it(`returns a DefaultEnvironmentStore with a custom initial state`, () => {
+  it(`(initialState) returns a DefaultEnvironmentStore with initial state`, () => {
     const initialState = { a: 0 };
     const store: any = createEnvironmentStore(initialState);
     expect(store).toBeInstanceOf(DefaultEnvironmentStore);
