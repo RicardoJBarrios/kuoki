c412f1a9e06d0e5ed7bf61327a9f5cf96d7be206
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-04-03 20:09:58 +0100

    docs(environment): update query config documentation

--- packages/environment/src/lib/query/environment-query-config-factory.function.ts
+++ packages/environment/src/lib/query/environment-query-config-factory.function.ts
@@ -6,7 +6,6 @@ import { EnvironmentQueryConfig } from './environment-query-config.interface';
  * Returns the environment query configuration with default values for empty parameters.
  * @param config The partial environment query config.
  * @returns The environment query configuration with all default values for empty parameters.
- * @see {@link EnvironmentQueryConfig}
  */
 export function environmentQueryConfigFactory(
   config?: EnvironmentQueryConfig | null
