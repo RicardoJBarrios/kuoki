54c5cd098377b23fd1c3796950f94a6e83290496
RicardoJBarrios <ricardojbarrios@gmail.com>
2022-04-11 11:13:59 +0100

    fix(environment-angular): temp fix lint

--- packages/environment-angular/src/lib/helpers/get-options-factory.function.ts
+++ packages/environment-angular/src/lib/helpers/get-options-factory.function.ts
@@ -1,5 +1,6 @@
 import { GetOptions } from '@kuoki/environment';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getOptionsFactory<T, K extends GetOptions<any>>(options?: K): GetOptions<T> {
   return {
     defaultValue: options?.defaultValue,
